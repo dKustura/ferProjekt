3d248fa584992633689e42685e18d66646a4c12d
Bruno Kantura <bkantura@outlook.com>
2016-10-30 18:46:04 +0100

    Added album model

---
+++ models/album.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const albumSchema = new Schema({
+
+    user: {
+       type: ObjectId,
+       ref: 'User'
+    },
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: String,
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+
+    photos: [{
+        type: ObjectId,
+        ref: 'Photo'
+    }],
+
+});
+
+module.exports = mongoose.model ('Photo-Album', albumSchema);
