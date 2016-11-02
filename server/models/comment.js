d392a955967061ccd0b1631c1d1c6ef84d5ec81d
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2016-11-02 13:09:07 +0100

    Fix indention

---
+++ server/models/comment.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const MAX_LENGTH = 8000;
+
+const commentSchema = Schema({
+  content: {
+    type: String,
+    max: [MAX_LENGTH, `Comment must not be longer than ${MAX_LENGTH} characters`],
+    required: [true, 'Comment content is required']
+  },
+
+  user: {
+    type: ObjectId,
+    ref: 'User'
+  },
+
+  likes: [{
+    type: ObjectId,
+    ref: 'User'
+  }],
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
