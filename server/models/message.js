3f27d9859ac063d94fea3709d18d619307356bd1
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2016-11-04 21:46:21 +0100

    Fix indentation in message model

---
+++ server/models/message.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const MAX_LENGTH = 20000;
+
+const messageSchema = new Schema({
+
+  content: {
+    type: String,
+    required: [true, 'Message content is required'],
+    max: [MAX_LENGTH, 'Message must not be longer than ${MAX_LENGTH} characters']
+  },
+
+  sentAt: {
+    type: Date,
+    default: Date.now()
+  },
+
+  sender: {
+    type: ObjectId,
+    ref: 'User'
+  },
+
+  receiver: {
+    type: ObjectId,
+    ref: 'User'
+  }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
