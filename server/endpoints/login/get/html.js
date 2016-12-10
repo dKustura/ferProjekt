8b778b8452aac9c43f5253f11fc3470ab5b791f9
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2016-12-10 11:35:38 +0100

    Refactor login logic

--- server/endpoints/login/get/html.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(req, res) {
-  res.setHeader('Content-Type', 'text/html');
-
-  // this will render login template in /server/views
-  res.render('login');
-}
--- server/endpoints/login/index.js
+++ server/endpoints/login/index.js
@@ -1,27 +1,30 @@
 const express = require('express');
-const accepts = require('accepts');
-
-const getHtml = require('./get/html');
-const getJson = require('./get/json');
-
-const postJson = require('./post/json');
 
 const router = new express.Router();
 
 router.get('/login', function(req, res) {
-  const accept = accepts(req);
-  switch(accept.type(['json', 'html'])) {
-  case 'json':
-    getJson(req, res);
-    break
-  default:
-    getHtml(req, res);
-    break
-  }
+
+  // this will render login template in /server/views
+  res.render('login');
 });
 
 router.post('/login', function(req, res) {
-  postJson(req, res);
+  passport.authenticate('local-login', (error, user, info) => {
+    if (error) {
+      res.status(401);
+      res.send(error);
+      return;
+    }
+
+    if (!user) {
+      res.status(404);
+      res.send(info);
+      return;
+    }
+
+    res.status(200);
+    res.send(user);
+  })(req, res);
 });
 
 module.exports = router;
