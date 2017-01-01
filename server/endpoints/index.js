144ab5cdbf5be5e6aeb7a9f2d523144c6cb1b4f1
Luka Lazanja <luka.lazanja@fer.hr>
2017-01-01 16:44:49 +0100

    endpoint for photo comments/likes

--- server/endpoints/index.js
+++ server/endpoints/index.js
@@ -12,6 +12,7 @@ const userData = require('./user-data');
 const upload = require('./upload');
 const comment = require('./comment');
 const edit = require('./edit');
+const photo = require('./photo');
 
 const router = new express.Router();
 
@@ -37,5 +38,6 @@ router.use(isAuthenticated, userData);
 router.use(isAuthenticated, upload);
 router.use(isAuthenticated, comment);
 router.use(isAuthenticated, edit);
+router.use(isAuthenticated, photo);
 
 module.exports = router;
---
+++ server/endpoints/photo/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const Comment = require('../../models/comment');
+const Photo = require('../../models/photo');
+
+const router = new express.Router();
+
+router.post('/photo/:id/like', function(req, res) {
+  const currentUser = req.user;
+
+  Photo.findById(req.params.id, (err, photo) => {
+    if (photo.likes.indexOf(currentUser.id) === -1) {
+      photo.likes.push(currentUser);
+      photo.save((photoSaveError) => {
+        if (photoSaveError) {
+          res.send(photoSaveError);
+          return;
+        }
+      });
+    } else {
+      res.status(401);
+      res.send();
+      return;
+    }
+
+    res.redirect('back');
+  });
+});
+
+router.post('/photo/:id/unlike', function(req, res) {
+  const currentUser = req.user;
+
+  Photo.findById(req.params.id, (err, photo) => {
+    const userIndex = photo.likes.indexOf(currentUser.id);
+
+    if (userIndex !== -1) {
+      photo.likes.splice(userIndex);
+      photo.save((photoSaveError) => {
+        if (photoSaveError) {
+          res.send(photoSaveError);
+          return;
+        }
+      });
+    } else {
+      res.status(401);
+      res.send();
+      return;
+    }
+
+    res.redirect('back');
+  });
+});
+
+router.post('/photo/:id/comment', function(req, res) {
+  const currentUser = req.user;
+
+  Photo.findById(req.params.id, (photoError, photo) => {
+    if (photoError) {
+      res.send(photoError);
+      return;
+    }
+
+    const newComment = new Comment();
+    newComment.user = currentUser;
+    newComment.content = req.body.content;
+    newComment.post = photo;
+
+    newComment.save((commentError) => {
+      if (commentError) {
+        res.send(commentError);
+        return;
+      }
+
+      photo.comments.push(newComment);
+      photo.save((photoSaveError) => {
+        if (photoSaveError) {
+          res.send(photoSaveError);
+          return;
+        }
+
+        res.redirect('back');
+      });
+    });
+  });
+});
+
+module.exports = router;
