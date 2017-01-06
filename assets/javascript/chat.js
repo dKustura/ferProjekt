bf1e9dad73e27028fa2283d52d766679219e56e3
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-01-06 19:56:30 +0100

    Add styling for search

--- assets/javascript/chat.js
+++ assets/javascript/chat.js
@@ -4,16 +4,16 @@ $(function() {
 
   $.getJSON('/user_id', function(data) {
 
-    var otherUser = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
-    var currentUser = data;
-    var socket = io.connect({ query: `user=${currentUser}` });
+    const otherUser = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
+    const currentUser = data;
+    const socket = io.connect({query: `user=${currentUser}`});
     const $messageForm = $('.js-message-form');
     const $message = $('.js-message');
     const $chat = $('.chat');
 
     $messageForm.on('submit', (e) => {
       e.preventDefault();
-      socket.emit('send message', { content: $message.val(), sender: currentUser, receiver: otherUser});
+      socket.emit('send message', {content: $message.val(), sender: currentUser, receiver: otherUser});
       $message.val('');
     });
 
