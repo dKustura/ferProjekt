343bc1ce5bc3d107d29b86e3818a67de0544e360
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-01-07 15:58:36 +0100

    Fix scrolling

--- assets/javascript/chat.js
+++ assets/javascript/chat.js
@@ -1,31 +1,36 @@
 import $ from 'jquery';
 
 $(function() {
-
   const $chat = $('.chat');
-  $chat.get(0).scrollTop = $chat.get(0).scrollHeight;
 
-  $.getJSON('/user_id', function(data) {
+  function scrollToBottom() {
+    $chat.get(0).scrollTop = $chat.get(0).scrollHeight;
+  }
 
-    const otherUser = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
-    const currentUser = data;
-    const socket = io.connect({query: `user=${currentUser}`});
-    const $messageForm = $('.js-message-form');
-    const $message = $('.js-message');
+  if ($chat) {
+    scrollToBottom();
+    $.getJSON('/user_id', function(data) {
 
-    $messageForm.on('submit', (e) => {
-      e.preventDefault();
-      socket.emit('send message', {content: $message.val(), sender: currentUser, receiver: otherUser});
-      $message.val('');
-      $chat.get(0).scrollTop = $chat.get(0).scrollHeight;
+      const otherUser = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
+      const currentUser = data;
+      const socket = io.connect({query: `user=${currentUser}`});
+      const $messageForm = $('.js-message-form');
+      const $message = $('.js-message');
+
+      $messageForm.on('submit', (e) => {
+        e.preventDefault();
+        socket.emit('send message', {content: $message.val(), sender: currentUser, receiver: otherUser});
+        $message.val('');
+        scrollToBottom();
+      });
+
+      socket.on('new message', function(message) {
+        $chat.append(
+          `<div><b>${message.sender.firstName} ${message.sender.lastName}: </b> ${message.content}</div>`
+        );
+
+        scrollToBottom();
+      });
     });
-
-    socket.on('new message', function(message) {
-      $chat.append(
-        `<div><b>${message.sender.firstName} ${message.sender.lastName}: </b> ${message.content}</div>`
-      );
-
-      $chat.get(0).scrollTop = $chat.get(0).scrollHeight;
-    });
-  });
+  }
 });
